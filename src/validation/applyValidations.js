557da9412a3bbca8267fb5b9a4f10687c16eb9c9
CristianD.Gonzalez <cristiand.gonzalez094@gmail.com>
2024-08-09 16:00:22 -0300

    correccion

--- src/validation/applyValidations.js
+++ src/validation/applyValidations.js
@@ -4,7 +4,8 @@ export const applyValidations = (req, res, next) => {
     const errores = validationResult(req)
 
     if (!errores.isEmpty()) {
-        return res.status(400).json(errores)
+        const errors = errores.array().map(a => a.msg)
+        return res.status(400).json({ message: errors })
     }
 
     next()
