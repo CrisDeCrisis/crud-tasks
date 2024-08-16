fee7d315eda40416d309a5897560145e2db19fa1
CristianD.Gonzalez <cristiand.gonzalez094@gmail.com>
2024-08-16 16:33:21 -0300

    clase 16-8

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,26 @@
 import './style.css'
 
-const $app = document.querySelector('#app')
+const $app = document.getElementById('app')
 
-$app.innerHTML = `
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-`
+const API_URL = 'http://localhost:3000/api/tasks'
+
+async function fetchTasks() {
+  try {
+    const response = await fetch(API_URL)
+    const tasks = await response.json()
+
+    tasks.forEach(task => {
+      const taskElement = `
+        <div id='taskContainer' class="bg-amber-200 shadow-xl rounded p-4 mb-4">
+          <h2 class="text-yellow-700 text-xl font-bold mb-2">${task.title}</h2>
+          <p class="text-yellow-950">${task.description}</p>
+        </div>
+      `
+      $app.innerHTML += taskElement
+    })
+  } catch (error) {
+    console.error('Error fetching tasks:', error)
+  }
+}
+
+fetchTasks()
