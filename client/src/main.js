d9cde82487c6045fb9d2fcd136134d1b8e5e1725
dev-CristianD.Gonzalez <cristiand.gonzalez094@gmail.com>
2024-08-18 19:45:54 -0300

    se estilo el projecto con tailwindcss

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-const $app = document.getElementById('app')
+const $taskList = document.getElementById('task-list')
 
 const API_URL = 'http://localhost:3000/api/tasks'
 
@@ -9,14 +9,34 @@ async function fetchTasks() {
     const response = await fetch(API_URL)
     const tasks = await response.json()
 
+    // Limpiar la lista de tareas antes de agregar nuevas tareas
+    $taskList.innerHTML = ''
+
     tasks.forEach(task => {
-      const taskElement = `
-        <div id='taskContainer' class="bg-amber-200 shadow-xl rounded p-4 mb-4">
-          <h2 class="text-yellow-700 text-xl font-bold mb-2">${task.title}</h2>
-          <p class="text-yellow-950">${task.description}</p>
-        </div>
+      const taskElement = document.createElement('div')
+      taskElement.className = 'task-container p-4 rounded-md shadow-lg flex flex-col justify-between'
+
+      taskElement.innerHTML = `
+        <section class='flex justify-between items-start'>
+          <div>
+            <h2 class='text-lg font-semibold text-left'>${task.title}</h2>
+            <p class='text-gray-400 text-left'>${task.description}</p>
+          </div>
+          <span class='text-sm ${task.isComplete ? 'text-green-500' : 'text-red-500'}'>
+            ${task.isComplete ? 'Completada' : 'Pendiente'}
+          </span>
+        </section>
+        <section class='flex justify-end mt-4'>
+          <button class='modify-btn bg-blue-600 hover:bg-blue-800 text-white font-bold py-1 px-2 rounded'>
+            Modificar
+          </button>
+          <button class='delete-btn bg-red-600 hover:bg-red-800 text-white font-bold py-1 px-2 rounded ml-2'>
+            Eliminar
+          </button>
+        </section>
       `
-      $app.innerHTML += taskElement
+
+      $taskList.appendChild(taskElement)
     })
   } catch (error) {
     console.error('Error fetching tasks:', error)
